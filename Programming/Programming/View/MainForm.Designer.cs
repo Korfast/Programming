97409eaed8be6fc716f7410d3a5a859a4bf73ea6
Korfast <fastkorny@gmail.com>
2025-03-04 14:51:21 +0700

    Step 7

--- Programming/Programming/View/MainForm.Designer.cs
+++ Programming/Programming/View/MainForm.Designer.cs
@@ -28,20 +28,132 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.EnumsTabPage = new System.Windows.Forms.TabPage();
+            this.MainTabControl = new System.Windows.Forms.TabControl();
+            this.EnumsLstBox = new System.Windows.Forms.ListBox();
+            this.EnumsGroupBox = new System.Windows.Forms.GroupBox();
+            this.ValuesListBox = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.EnumsLabel = new System.Windows.Forms.Label();
+            this.ValuesLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EnumsTabPage.SuspendLayout();
+            this.MainTabControl.SuspendLayout();
+            this.EnumsGroupBox.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // EnumsTabPage
+            // 
+            this.EnumsTabPage.Controls.Add(this.EnumsGroupBox);
+            this.EnumsTabPage.Location = new System.Drawing.Point(4, 22);
+            this.EnumsTabPage.Name = "EnumsTabPage";
+            this.EnumsTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.EnumsTabPage.Size = new System.Drawing.Size(792, 424);
+            this.EnumsTabPage.TabIndex = 0;
+            this.EnumsTabPage.Text = "Enums";
+            this.EnumsTabPage.UseVisualStyleBackColor = true;
+            // 
+            // MainTabControl
+            // 
+            this.MainTabControl.Controls.Add(this.EnumsTabPage);
+            this.MainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainTabControl.Location = new System.Drawing.Point(0, 0);
+            this.MainTabControl.Name = "MainTabControl";
+            this.MainTabControl.SelectedIndex = 0;
+            this.MainTabControl.Size = new System.Drawing.Size(800, 450);
+            this.MainTabControl.TabIndex = 0;
+            // 
+            // EnumsLstBox
+            // 
+            this.EnumsLstBox.FormattingEnabled = true;
+            this.EnumsLstBox.Location = new System.Drawing.Point(20, 35);
+            this.EnumsLstBox.Name = "EnumsLstBox";
+            this.EnumsLstBox.Size = new System.Drawing.Size(150, 199);
+            this.EnumsLstBox.TabIndex = 0;
+            // 
+            // EnumsGroupBox
+            // 
+            this.EnumsGroupBox.Controls.Add(this.label1);
+            this.EnumsGroupBox.Controls.Add(this.ValuesLabel);
+            this.EnumsGroupBox.Controls.Add(this.EnumsLabel);
+            this.EnumsGroupBox.Controls.Add(this.textBox1);
+            this.EnumsGroupBox.Controls.Add(this.ValuesListBox);
+            this.EnumsGroupBox.Controls.Add(this.EnumsLstBox);
+            this.EnumsGroupBox.Location = new System.Drawing.Point(6, 6);
+            this.EnumsGroupBox.Name = "EnumsGroupBox";
+            this.EnumsGroupBox.Size = new System.Drawing.Size(672, 271);
+            this.EnumsGroupBox.TabIndex = 1;
+            this.EnumsGroupBox.TabStop = false;
+            this.EnumsGroupBox.Text = "Enumerations";
+            // 
+            // ValuesListBox
+            // 
+            this.ValuesListBox.FormattingEnabled = true;
+            this.ValuesListBox.Location = new System.Drawing.Point(187, 35);
+            this.ValuesListBox.Name = "ValuesListBox";
+            this.ValuesListBox.Size = new System.Drawing.Size(150, 199);
+            this.ValuesListBox.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(356, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(150, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // EnumsLabel
+            // 
+            this.EnumsLabel.AutoSize = true;
+            this.EnumsLabel.Location = new System.Drawing.Point(17, 16);
+            this.EnumsLabel.Name = "EnumsLabel";
+            this.EnumsLabel.Size = new System.Drawing.Size(109, 13);
+            this.EnumsLabel.TabIndex = 3;
+            this.EnumsLabel.Text = "Choose enumerations";
+            // 
+            // ValuesLabel
+            // 
+            this.ValuesLabel.AutoSize = true;
+            this.ValuesLabel.Location = new System.Drawing.Point(184, 16);
+            this.ValuesLabel.Name = "ValuesLabel";
+            this.ValuesLabel.Size = new System.Drawing.Size(72, 13);
+            this.ValuesLabel.TabIndex = 4;
+            this.ValuesLabel.Text = "Choose value";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(353, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "ht value:";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MainTabControl);
             this.Name = "MainForm";
-            this.Text = "MainForm";
+            this.Text = "ProgrammingDemo";
+            this.EnumsTabPage.ResumeLayout(false);
+            this.MainTabControl.ResumeLayout(false);
+            this.EnumsGroupBox.ResumeLayout(false);
+            this.EnumsGroupBox.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.TabPage EnumsTabPage;
+        private System.Windows.Forms.TabControl MainTabControl;
+        private System.Windows.Forms.ListBox EnumsLstBox;
+        private System.Windows.Forms.GroupBox EnumsGroupBox;
+        private System.Windows.Forms.ListBox ValuesListBox;
+        private System.Windows.Forms.Label EnumsLabel;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label ValuesLabel;
+        private System.Windows.Forms.Label label1;
     }
 }
 
