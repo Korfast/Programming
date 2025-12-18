0ac905f6a89779cef6260c0affb5a0fde890f21b
Korfast <fastkorny@gmail.com>
2025-12-18 21:57:42 +0700

    The Tasks/5_Polymorphism branch has been created.

---
+++ src/ObjectOrientedPractics/ObjectOrientedPractics/Model/Classes/PointsDiscount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedPractics.Model
+{
+    public class PointsDiscount
+    {
+        private 
+
+        public double Calculate(List<Item> items)
+        {
+
+        }
+
+        public double Apply(List<Item> items)
+        {
+
+        }
+
+        public void Update(List<Item> items)
+        {
+
+        }
+    }
+}
